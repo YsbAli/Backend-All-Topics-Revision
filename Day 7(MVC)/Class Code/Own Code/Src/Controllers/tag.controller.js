ae43c68b2dc13dc69f5d1d7fbd42efcb73c86907
YsbAli <yousub.webdev@gmail.com>
2023-09-17 13:08:19 +0530

    modify tagController

--- Day 7(MVC)/Class Code/Own Code/Src/Controllers/tag.controller.js
+++ Day 7(MVC)/Class Code/Own Code/Src/Controllers/tag.controller.js
@@ -1,81 +1,116 @@
-//----------------------------------------------------------- TAG CRUD Starts -------------------------------------------------------
+// //--------------------------------- TAG CRUD Starts -----------------------------
 
 
-/*
-Working with tags collection ( Endpoints / route )
+// /*
+// Working with tags collection ( Endpoints / route )
 
-GET --> get/tags
-GET SIGLE ITEM ---> get/tags/:id
-POST --> post/tags
-UPDATE SINGLE ITEM ---> patch/tags/:id
-DELETE SINGLE ITEM ---> delete/tags/:id
+// GET --> get/tags
+// GET SIGLE ITEM ---> get/tags/:id
+// POST --> post/tags
+// UPDATE SINGLE ITEM ---> patch/tags/:id
+// DELETE SINGLE ITEM ---> delete/tags/:id
 
-*/
+// */
 
-const express = require('express')
-const router = express.Router()
+// const Tag = require("../module/tag.model")
+// const express = require('express')
+// const router = express.Router()
+
+
+// router.post('/', async (req, resp) => {
+
+//     try {
+//         const tag = await Tag.create(req.body)
+//         return resp.send(tag)
+//     } catch (e) {
+//         return resp.status(500).send(e.message)
+//     }
+
+// })
+
+
+// router.get("", async (req, resp) => {
+//     try {
+//         const tags = await Tag.find().lean().exec()
+//         return resp.send(tags)
+//     }
+//     catch (e) {
+//         return resp.status(500).send(e.message)
+//     }
+// })
+
+// // finding one tag
+
+// router.get("/:id", async (req, resp) => {
+
+//     try {
+//         const tag = await Tag.findById(req.params.id).lean().exec()
+//         return resp.send(tag)
+//     }
+//     catch (e) {
+//         return resp.status(500).send(e.message)
+//     }
+// })
+
+
+// router.patch("/:id", async (req, resp) => {
+//     try {
+//         const tag = await Tag.findByIdAndUpdate(req.params.id, req.body, { new: true })
+//         return resp.send(tag)
+//     }
+//     catch (e) {
+//         return resp.status(500).send(e.message)
+//     }
+// })
+
+
+
+// router.delete("/:id", async (req, resp) => {
+//     try {
+//         const tag = await Tag.findByIdAndDelete(req.params.id)
+//         return resp.send(tag)
+//     }
+//     catch (e) {
+//         return resp.status(500).send(e.message)
+//     }
+// })
+
+
+
+// module.exports = router;
+
+//------------------------------------- TAG CRUD Ends --------------------------
+
+
+
+// We can refactor our code : - 
+
+
+
+
 const Tag = require('../module/tag.model')
 
+const express = require('express')
 
-router.post('/', async (req, resp) => {
-    try {
-        const tag = await Tag.create(req.body)
-        return resp.send(tag)
-    } catch (e) {
-        return resp.status(500).send(e.message)
-    }
+const router = express.Router()
 
-})
+const CrudController = require('./Crud.controllers')
 
 
-router.get("/", async (req, resp) => {
-    try {
-        const tags = await Tag.find().lean().exec()
-        return resp.send(tags)
-    }
-    catch (e) {
-        return resp.status(500).send(e.message)
-    }
-})
+router.post('/', CrudController(Tag).post)
 
-// finding one tag
+//getall
+router.get('/', CrudController(Tag).GetAll)
 
-router.get("/:id", async (req, resp) => {
+//getOne
+router.get('/:id', CrudController(Tag).GetOne)
 
-    try {
-        const tag = await Tag.findById(req.params.id).lean().exec()
-        return resp.send(tag)
-    }
-    catch (e) {
-        return resp.status(500).send(e.message)
-    }
-})
+//UpdateOne
+router.patch('/:id', CrudController(Tag).updateOne)
 
-
-router.patch("/:id", async (req, resp) => {
-    try {
-        const tag = await Tag.findByIdAndUpdate(req.params.id, req.body, { new: true })
-        return resp.send(tag)
-    }
-    catch (e) {
-        return resp.status(500).send(e.message)
-    }
-})
-
-
-
-router.delete("/:id", async (req, resp) => {
-    try {
-        const tag = await Tag.findByIdAndDelete(req.params.id)
-        return resp.send(tag)
-    }
-    catch (e) {
-        return resp.status(500).send(e.message)
-    }
-})
-
-
+// deleteOne
+router.delete("/:id", CrudController(Tag).deleteOne)
 
 module.exports = router;
 
-//----------------------------------------------------------- TAG CRUD Ends -------------------------------------------------------
+
