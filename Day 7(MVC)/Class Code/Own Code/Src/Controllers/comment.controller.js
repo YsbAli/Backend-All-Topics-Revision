2379a0968ba28c00b1f416d636c241f75a6290db
YsbAli <yousub.webdev@gmail.com>
2023-09-17 13:15:01 +0530

    Modify CommentController

--- Day 7(MVC)/Class Code/Own Code/Src/Controllers/comment.controller.js
+++ Day 7(MVC)/Class Code/Own Code/Src/Controllers/comment.controller.js
@@ -20,68 +20,99 @@ DELETE SINGLE ITEM ---> delete/commands/:id
 
 */
 
-const express = require('express')
-const router = express.Router()
-const Comment = require('../module/comment.model')
 
-router.post("", async (req, resp) => {
-    try {
-        const commends = await Comment.create(req.body)
-        return resp.send(commends)
-    }
-    catch (e) {
-        return resp.status(500).send(e.message)
-    }
-})
+// const Comment = require('../module/comment.model')
+// const express = require('express')
+// const router = express.Router()
 
-
-router.get("/", async (req, resp) => {
-    try {
-        const commends = await Comment.find().lean().exec()
-        return resp.send(commends)
-    } catch (e) {
-        return resp.status(500).send(e.message)
-    }
-})
-
-router.get("/:id", async (req, resp) => {
-    try {
-        const commends = await Comment.findById(req.params.id).lean().exec()
-        return resp.send(commends)
-    }
-    catch (e) {
-        return resp.status(500).send(e.message)
-    }
-})
+// router.post('', async (req, resp) => {
+//     try {
+//         const comments = await Comment.create(req.body)
+//         return resp.send(comments)
+//     }
+//     catch (e) {
+//         return resp.status(500).send(e.message)
+//     }
+// })
 
 
-router.patch('/:id', async (req, resp) => {
-    try {
-        const commends = await Comment.findByIdAndUpdate(req.params.id, req.body, { new: true })
-        return resp.send(commends)
-    }
-    catch (e) {
-        return resp.status(500).send(e.message)
-    }
+// router.get("/", async (req, resp) => {
+//     try {
+//         const comments = await Comment.find().lean().exec()
+//         return resp.send(comments)
+//     } catch (e) {
+//         return resp.status(500).send(e.message)
+//     }
+// })
 
-})
-
-
-
-router.delete("/:id", async (req, resp) => {
-    try {
-        const commends = await Comment.findByIdAndDelete(req.params.id).lean().exec()
-
-        return resp.send(commends)
-    }
-    catch (e) {
-        return resp.status(500).send(e.message)
-    }
-})
+// router.get("/:id", async (req, resp) => {
+//     try {
+//         const comments = await Comment.findById(req.params.id).lean().exec()
+//         return resp.send(comments)
+//     }
+//     catch (e) {
+//         return resp.status(500).send(e.message)
+//     }
+// })
 
 
-module.exports = router;
+// router.patch('/:id', async (req, resp) => {
+//     try {
+//         const comments = await Comment.findByIdAndUpdate(req.params.id, req.body, { new: true })
+//         return resp.send(comments)
+//     }
+//     catch (e) {
+//         return resp.status(500).send(e.message)
+//     }
+
+// })
+
+
+
+// router.delete("/:id", async (req, resp) => {
+//     try {
+//         const comments = await Comment.findByIdAndDelete(req.params.id).lean().exec()
+
+//         return resp.send(comments)
+//     }
+//     catch (e) {
+//         return resp.status(500).send(e.message)
+//     }
+// })
+
+
+// module.exports = router;
 
 
 //-------------- COMMEND CRUD ENDS -----------------------
+
+
+
+
+// Now Fresh and refactoring code ....
+
+const Comment = require('../module/comment.model')
+
+const express = require('express')
+
+const router = express.Router()
+
+const CrudController = require('./Crud.controllers')
+
+
+router.post('/', CrudController(Comment).post)
+
+//getall
+router.get('/', CrudController(Comment).GetAll)
+
+//getOne
+router.get('/:id', CrudController(Comment).GetOne)
+
+//UpdateOne
+router.patch('/:id', CrudController(Comment).updateOne)
+
+// deleteOne
+router.delete("/:id", CrudController(Comment).deleteOne)
+
+module.exports = router;
 
