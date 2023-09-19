051582bf7c214a45f9b53003657b707d28df9218
YsbAli <yousub.webdev@gmail.com>
2023-09-20 00:52:22 +0530

    all code - last commit of own/class code

--- Day 10 (File Uploads)/Class Code/src/controllers/user.controller.js
+++ Day 10 (File Uploads)/Class Code/src/controllers/user.controller.js
@@ -1,79 +1,79 @@
 
-// //  ---------------------This is for profile_pic upload commit ---------------------
+
+/*
+
+const express = require('express')
+
+const User = require("../models/user.model")
+
+const router = express.Router()
+
+// const path = require("path")
+
+const Uploads = require('../middleware/FileUpload')
+
+
+//for single version
+router.post("/single", Uploads.single("profile_pic"), async (req, resp) => {
+    try {
+        // console.log(req.file)             // it's an object
+        const user = await User.create({
+            id: req.body.id,
+
+            first_Name: req.body.first_Name,
+
+            last_Name: req.body.last_Name,
+
+            email: req.body.email,
+
+            password: req.body.password,
+
+            age: req.body.age,
+
+            profile_pic: req.file.path,           //here req.file.path  (path is for file path)
+        })
+        return resp.status(201).send(user)
+    }
+    catch (err) {
+        return resp.status(500).send(err.message)
+    }
+})
+
+
+//multiple version
+router.post("/multiple", Uploads.any("profile_pic"),async (req, resp) => {
+
+    try {
+        const filePaths = req.files.map(file=> file.path)              // this is for the each file path,,,
+        const user = await User.create({
+            id: req.body.id,
+
+            first_Name: req.body.first_Name,
+
+            last_Name: req.body.last_Name,
+
+            email: req.body.email,
+
+            password: req.body.password,
+
+            age: req.body.age,
+
+            // profile_pic: req.file.path,           //here req.file.path  (path is for file path)
+            profile_pic: filePaths,                // we are giving the each file path inside the filePaths variable
+        })
+        return resp.status(201).send(user)
+    }
+    catch (err) {
+        return resp.status(500).send(err.message)
+    }
+})
+
+
+module.exports = router;
 
 
 
-// const express = require('express')
-
-// const User = require("../models/user.model")
-
-// const router = express.Router()
-
-// // const path = require("path")
-
-// const Uploads = require('../middleware/FileUpload')
-
-
-// //for single version
-// router.post("/single", Uploads.single("profile_pic"), async (req, resp) => {
-//     try {
-//         const user = await User.create({
-//             id: req.body.id,
-
-//             first_Name: req.body.first_Name,
-
-//             last_Name: req.body.last_Name,
-
-//             email: req.body.email,
-
-//             password: req.body.password,
-
-//             age: req.body.age,
-
-//             profile_pic: req.file.path,           //here req.file.path  (path is for file path)
-//         })
-//         return resp.status(201).send(user)
-//     }
-//     catch (err) {
-//         return resp.status(500).send(err.message)
-//     }
-// })
-
-
-
-// //multiple version
-// router.post("/multiple", Uploads.any("profile_pic"),async (req, resp) => {
-
-//     try {
-//         const filePaths = req.files.map(file=> file.path)              // this is for the each file path,,,
-//         const user = await User.create({
-//             id: req.body.id,
-
-//             first_Name: req.body.first_Name,
-
-//             last_Name: req.body.last_Name,
-
-//             email: req.body.email,
-
-//             password: req.body.password,
-
-//             age: req.body.age,
-
-//             // profile_pic: req.file.path,           //here req.file.path  (path is for file path)
-//             profile_pic: filePaths,                // we are giving the each file path inside the filePaths variable
-//         })
-//         return resp.status(201).send(user)
-//     }
-//     catch (err) {
-//         return resp.status(500).send(err.message)
-//     }
-// })
-
-
-// module.exports = router;
-
-
-
+*/
 
 
 
