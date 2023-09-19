bdee839ab8606c0f522995e5f0cf343a789d0629
YsbAli <yousub.webdev@gmail.com>
2023-09-20 02:50:18 +0530

    11 - Authentication - setup and auth.controller created - user register steps

---
+++ Day 11 (Authentication)/Class Code/Own Code/src/controllers/auth.controller.js
@@ -0,0 +1,66 @@
+
+const User = require("../models/user.model")
+
+
+const Register = async (req, resp) => {
+    try {
+
+        // we will try to find the user with the email provided
+        let user = await User.findOne({ email: req.body.email }).lean().exec()
+
+
+        //if user is found then it is an error
+        if (user) {
+            return resp.status(400).send({ message: "This Email is Allready Exits! Please try another email" })
+        }
+
+
+        //if user is not found then we will create the user with the email and the password provided
+        user = await User.create(req.body)
+
+
+        //then we will hash the password to make the password more secure (besically we encrype the password so that no one can see it)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        resp.send(user)
+    }
+    catch (err) {
+        return resp.status(500).send(err.message)
+    }
+}
+
+
+
+const Login = (req, resp) => {
+    try {
+        resp.send("Login")
+    }
+    catch (err) {
+        return resp.status(500).send(err.message)
+    }
+}
+
+
+module.exports = { Register, Login }
