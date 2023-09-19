235d3a654b47a0dd56e88d9592b91f928c625634
YsbAli <yousub.webdev@gmail.com>
2023-09-19 14:14:50 +0530

    custom error message to all fields

--- Day 9 (Validation)/Class Code/src/controllers/user.controller.js
+++ Day 9 (Validation)/Class Code/src/controllers/user.controller.js
@@ -159,17 +159,95 @@ const { body, validationResult } = require('express-validator');
 
 //Password Validation customly using Regex
 
+// router.post("",
+
+//     // body("id").isNumeric().custom(async (value) => {
+//     //     const user = await User.findOne({ id: value });
+//     //     if (user) {
+//     //         throw new Error("Id already exists")
+//     //     }
+//     //     return true;
+
+//     // }),
+
+//     //   body("password").isStrongPassword()                      //----> it's given by express-validator
+
+//     //  use this inside /-----/   ------> ^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$
+
+//     //    like this ----> /  ^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$ /
+
+//     body("password").isLength({ min: 8, max: 20 }).custom((value) => {
+
+//         let pattern = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
+
+//         if (pattern.test(value)) {
+//             return true;
+//         }
+//         throw new Error("Password is Not Storng")
+
+//     }),
+
+
+//     async (req, resp) => {
+//         try {
+//             const errors = validationResult(req);
+//             // error = []
+//             if (!errors.isEmpty()) {
+//                 return resp.status(400).json({ errors: errors.array() });
+//             }
+
+//             return resp.send("Working")
+//         }
+//         catch (err) {
+//             return resp.send(err.message)
+//         }
+//     })
+
+
+
+// Now See Custom Message -----> .withMessage("Error Messag")    .bail() ---> this will skip() next code
 
 router.post("",
 
-    // body("id").isNumeric().custom(async (value) => {
-    //     const user = await User.findOne({ id: value });
-    //     if (user) {
-    //         throw new Error("Id already exists")
-    //     }
-    //     return true;
 
-    // }),
+   //for id
+    body("id").isNumeric().withMessage("Id should not be a Number!").bail()
+        .custom(async (value) => {
+            const user = await User.findOne({ id: value });
+            if (user) {
+                throw new Error("Id already exists")
+            }
+            return true;
+
+        }),
+
+
+
+
+
+        //for first_Name
+    body("first_Name").isEmpty().isLowercase().isLength({ min: 3, max: 20 }).withMessage("First Name should have 3 to 20 charaters long"),
+    
+    //for last_Name
+    body("last_Name").isLowercase().isLength({ min: 3, max: 20 }).withMessage("First Name should have 3 to 20 charaters long"),
+
+    // for ip_address
+    body("ip_address").notEmpty().isIP(),                      
+
+    //for age
+    body("age").isNumeric(),                 
+
+    //for birth_dateor
+    body("birth_date").isDate(),            
+
+    //for email validation and duplicate email 
+    body("email").isEmail().custom(async (value) => {
+        const user = await User.findOne({ email: value });
+        if (user) {
+            throw new Error("E-mail is already exists")
+        }
+
+    }),
 
     //   body("password").isStrongPassword()                      //----> it's given by express-validator
 
@@ -203,6 +281,11 @@ router.post("",
             return resp.send(err.message)
         }
     })
+
+
+
+
+
 
 
 
