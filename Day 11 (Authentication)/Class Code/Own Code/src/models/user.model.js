78dbea9586e4d2b256a741c6b36a021656cbf800
YsbAli <yousub.webdev@gmail.com>
2023-09-20 13:26:41 +0530

    Fresh code Register and Login

--- Day 11 (Authentication)/Class Code/Own Code/src/models/user.model.js
+++ Day 11 (Authentication)/Class Code/Own Code/src/models/user.model.js
@@ -1,25 +1,5 @@
 
-// const mongoose = require('mongoose')
-
-// const UserSchema = new mongoose.Schema({
-//     email: { type: String, required: true, unique: true },
-//     password: { type: String, required: true },
-// },
-//     {
-//         versionKey: false,
-//         timestamps: true
-
-//     }
-// )
-
-// module.exports = mongoose.model("user", UserSchema)    //userdata: userdatas
-
-
-// _______________________________________________________________________________________--
-
-
-
-
+/*
 
 const mongoose = require('mongoose')
 
@@ -37,6 +17,8 @@ const UserSchema = new mongoose.Schema({
 
     }
 )
+
+*/
 
 
 // ------------------------Hashing Part-------------------
@@ -68,6 +50,7 @@ UserSchema.pre("save", function (next) {
 
 // ------------------------ Fresh Code ~ Hashing Part-------------------
 
+
 /*
 
 UserSchema.pre('save', function (next) {
@@ -95,26 +78,70 @@ UserSchema.pre('save', function (next) {
 // }
 
 
-// .........Fresh Code
+// Fresh Code
+
+// UserSchema.methods.CheckPassword = function (password){
+//     return bcrypt.compareSync(password, this.password)
+
+// }
+
+
+
+
+
+// const User = mongoose.model("userauth", UserSchema)    //userdata: userdatas
+
+// module.exports = User
+
+
+
+
+
+
+// __________________________FRESH CODE ALL___________________________
+
+
+
+const mongoose = require('mongoose')
+
+const bcrypt = require('bcryptjs')
+
+
+//userModel
+const UserSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+},
+    {
+        versionKey: false,
+        timestamps: true
+
+    }
+)
+
+//Hasging Password
+UserSchema.pre('save', function (next) {
+
+    if (!this.isModified("password")) return next()
+
+    var hashPassword = bcrypt.hashSync(this.password, 8)
+
+    this.password = hashPassword;                          // user given password is --> ysb@1232 --- hashing Password is --> $2a$08$TK2QcljCYmeg2etH2IPXJu0WGHPZM2hwW18GbdxCnN1UpffA7p9Ky
+
+    return next()
+})
+
+//password authentication or password cheching
 
 UserSchema.methods.CheckPassword = function (password){
     return bcrypt.compareSync(password, this.password)
 
 }
 
+
 const User = mongoose.model("userauth", UserSchema)    //userdata: userdatas
 
 module.exports = User
-
-
-
-
-
-
-
-
-
-
 
 
 
