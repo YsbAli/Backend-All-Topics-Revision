71f5956c71dab1e01b128f3b6f8f3fd5ee2f082f
YsbAli <yousub.webdev@gmail.com>
2023-09-21 14:03:27 +0530

    adding jwt token to google Oauth

--- Day 12 (Oauth & Authorization)/Class Code/Own Code/src/index.js
+++ Day 12 (Oauth & Authorization)/Class Code/Own Code/src/index.js
@@ -58,7 +58,6 @@
 
 
 
-
 const express = require('express')
 const app = express()
 
@@ -73,8 +72,10 @@ const ProductController = require("./controllers/product.controller")
 //importing passport from gooleOauth file
 const passport = require('./configs/gooleOauth')
 
+// const { Register, Login,  } = require("./controllers/auth.controller")
 
-const { Register, Login } = require("./controllers/auth.controller")
+// importing newToken for token
+const { Register, Login, newToken } = require("./controllers/auth.controller")
 
 
 //we are directly using the method here,,,don't use kind of middleware
@@ -111,12 +112,37 @@ app.get('/auth/google',
 
 
 app.get('/auth/google/callback',
-    passport.authenticate('google', { failureRedirect: '/login' }),
-    function (req, res) {
-        // Successful authentication, redirect home.
 
-        // res.redirect('/');
-        return res.send(req.user)
+    passport.authenticate('google', { failureRedirect: '/login' }),                //failure middleware
+
+    //success middleware
+
+
+    // function (req, res) {
+    //     // Successful authentication, redirect home.
+    //     // res.redirect('/');                         //redirecting home page
+    //     // return res.send(req.user)                 //returning the user details
+
+    //     return res.send(req.user)                 //returning the user details
+    // });
+
+
+    // jwt token added and returinig the token after succes
+
+    function (req, res) {
+        //now returing the jwt token 
+        // const token = newToken(req.user)
+
+        // res.send({ user: req.user, token })
+
+        // return res.send(req.user)                 //returning the user details
+
+        //optimazing code,,, destructuring  {user}
+        const { user } = req;
+
+        const token = newToken(user)
+
+        return res.send({ user: user, token })
     });
 
 
@@ -130,13 +156,3 @@ app.listen(4012, async () => {
     }
     console.log("Server in running on port 4012")
 })
-
-
-
-
-
-
-
-
-
-
